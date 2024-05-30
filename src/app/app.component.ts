988d1899389285aa344d43a93017a10ffd90cb5b
Juan Enrique Realpe <jrealpec@hotmail.com>
2024-05-30 11:02:46 -0500

    20240530-JER. Implementacion inicial Proyecto KUPI Frontend Angular, TypeScript, Primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+
+
+interface TpId {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
+
+
 export class AppComponent {
-  title = 'kupi15';
+  value:string = 'kupi15';
+  email:string = 'email';
+  passwd:string = 'password';
 }
+
+/*
+export class DropdownBasicDemo implements OnInit {
+  tpId: TpId[] | undefined;
+
+  selectedTpid: TpId | undefined;
+
+  ngOnInit() {
+      this.tpId = [
+          { name: 'Nit', code: 'NI' },
+          { name: 'Cedula de ciudadania', code: 'CC' },
+          { name: 'Cedula de extranjeria', code: 'CE' },
+          { name: 'Tarjeta de identidad', code: 'TI' },
+          { name: 'Permiso especial de permanencia', code: 'PE' },
+          { name: 'Permiso temporal de permanencia', code: 'PT'}
+      ];
+  }
+}
+*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+import { DropdownModule } from 'primeng/dropdown';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    FormsModule,
+    PasswordModule,
+    DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
